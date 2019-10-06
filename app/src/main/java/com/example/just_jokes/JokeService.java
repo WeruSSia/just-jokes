3ccf4790e0981cfc175d46bd37bb7b46c9d2f15e
Weronika Mirek <werussia16@gmail.com>
2019-10-07 00:02:46 +0200

    JokeService implementation

--- app/src/main/java/com/example/just_jokes/JokeService.java
+++ app/src/main/java/com/example/just_jokes/JokeService.java
@@ -1,8 +1,6 @@
 package com.example.just_jokes;
 
 import retrofit2.Call;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
@@ -11,6 +9,7 @@ import retrofit2.http.Query;
 
 interface JokeService {
 
+    String hostHeader = "x-rapidapi-host:joke3.p.rapidapi.com";
     String apiKeyHeader = "x-rapidapi-key:33271c12femshc43be3cbec9200cp164af8jsna6babdb58409";
     String contentTypeHeader = "content-type:application/x-www-form-urlencoded";
 
@@ -19,18 +18,14 @@ interface JokeService {
     Call<JokeDto> getRandomJoke(@Query("nsfw") boolean nsfw);
 
     @Headers(apiKeyHeader)
-    @GET
-    Call getJokeById(@Field("id") String id);
+    @GET("joke/{id}/")
+    Call<JokeDto> getJokeById(@Path("id") String id);
 
-    @Headers(apiKeyHeader)
-    @GET
-    Call getJokeVotes(@Field("property") String property, @Field("id") String id);
+    @Headers({hostHeader, apiKeyHeader, contentTypeHeader})
+    @POST("joke/{id}/downvote")
+    Call<JokeDto> downvoteJoke(@Path("id") String id);
 
-    @Headers(apiKeyHeader + "," + contentTypeHeader)
-    @POST
-    Call downvoteJoke(@Field("id") String id);
-
-    @Headers(apiKeyHeader + "," + contentTypeHeader)
-    @POST
-    Call upvoteJoke(@Field("id") String id);
+    @Headers({hostHeader, apiKeyHeader, contentTypeHeader})
+    @POST("joke/{id}/upvote")
+    Call<JokeDto> upvoteJoke(@Path("id") String id);
 }

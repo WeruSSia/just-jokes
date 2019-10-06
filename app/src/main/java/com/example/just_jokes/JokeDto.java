68e4c29ec0c34f8e0382e5aa4ebb38b863d044f4
Weronika Mirek <werussia16@gmail.com>
2019-10-06 22:06:21 +0200

    JokeDto implementation

---
+++ app/src/main/java/com/example/just_jokes/JokeDto.java
@@ -0,0 +1,49 @@
+package com.example.just_jokes;
+
+class JokeDto {
+    private String id;
+    private String content;
+    private Boolean nsfw;
+    private Integer upvotes;
+    private Integer downvotes;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Boolean getNsfw() {
+        return nsfw;
+    }
+
+    public void setNsfw(Boolean nsfw) {
+        this.nsfw = nsfw;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+
+    public void setUpvotes(Integer upvotes) {
+        this.upvotes = upvotes;
+    }
+
+    public Integer getDownvotes() {
+        return downvotes;
+    }
+
+    public void setDownvotes(Integer downvotes) {
+        this.downvotes = downvotes;
+    }
+}

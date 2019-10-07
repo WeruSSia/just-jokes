5d9a0050e58526831366039dc01c534c77cf3955
Weronika Mirek <werussia16@gmail.com>
2019-10-07 23:44:37 +0200

    JokeRequestHandler implementation

---
+++ app/src/main/java/com/example/just_jokes/JokeRequestHandler.java
@@ -0,0 +1,89 @@
+package com.example.just_jokes;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+class JokeRequestHandler {
+
+    private String jokeId;
+    private JokeService jokeService;
+    private Context context;
+    private TextView randomJokeTextView;
+    private TextView upvotesTextView;
+    private TextView downvotesTextView;
+
+    public JokeRequestHandler(Context context, TextView randomJokeTextView, TextView upvotesTextView, TextView downvotesTextView) {
+        setUpJokeService();
+        this.context = context;
+        this.randomJokeTextView = randomJokeTextView;
+        this.upvotesTextView = upvotesTextView;
+        this.downvotesTextView = downvotesTextView;
+
+    }
+
+    private void setUpJokeService() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://joke3.p.rapidapi.com/v1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        jokeService = retrofit.create(JokeService.class);
+    }
+
+    private Callback<JokeDto> getJokeDtoCallback() {
+        return new Callback<JokeDto>() {
+            @Override
+            public void onResponse(Call<JokeDto> call, Response<JokeDto> response) {
+                randomJokeTextView.setText(response.body().getContent());
+                upvotesTextView.setText(context.getString(R.string.upvotes, response.body().getUpvotes()));
+                downvotesTextView.setText(context.getString(R.string.downvotes, response.body().getDownvotes()));
+                jokeId = response.body().getId();
+            }
+
+            @Override
+            public void onFailure(Call<JokeDto> call, Throwable t) {
+                randomJokeTextView.setText(t.getMessage());
+            }
+        };
+    }
+
+    void setRandomJokeButtonOnClickListener(Button randomJokeButton) {
+        randomJokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getRandomJoke();
+            }
+        });
+    }
+
+    void setUpvoteButtonOnClickListener(Button upvoteButton) {
+        upvoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jokeService.upvoteJoke(jokeId).enqueue(getJokeDtoCallback());
+            }
+        });
+    }
+
+    void setDownvoteButtonOnClickListener(Button downvoteButton) {
+        downvoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jokeService.downvoteJoke(jokeId).enqueue(getJokeDtoCallback());
+            }
+        });
+    }
+
+    void getRandomJoke() {
+        jokeService.getRandomJoke().enqueue(getJokeDtoCallback());
+    }
+
+}
--- app/src/main/java/com/example/just_jokes/MainActivity.java
+++ app/src/main/java/com/example/just_jokes/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.just_jokes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView randomJokeTextView = findViewById(R.id.random_joke_text_view);
+        final TextView upvotesTextView = findViewById(R.id.upvotes_text_view);
+        final TextView downvotesTextView = findViewById(R.id.downvotes_text_view);
+        final Button randomJokeButton = findViewById(R.id.random_joke_button);
+        final Button upvoteButton = findViewById(R.id.upvote_button);
+        final Button downvoteButton = findViewById(R.id.downvote_button);
+
+        JokeRequestHandler jokeRequestHandler = new JokeRequestHandler(this,randomJokeTextView,upvotesTextView,downvotesTextView);
+        jokeRequestHandler.getRandomJoke();
+        jokeRequestHandler.setRandomJokeButtonOnClickListener(randomJokeButton);
+        jokeRequestHandler.setUpvoteButtonOnClickListener(upvoteButton);
+        jokeRequestHandler.setDownvoteButtonOnClickListener(downvoteButton);
     }
 }

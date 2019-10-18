856e85d0f6eb8d51c362d64057b313f4467143d5
Weronika Mirek <werussia16@gmail.com>
2019-10-18 14:30:39 +0200

    MainAcitivity implementation (managing fragments, tabLayout and viewPager)

--- app/src/main/java/com/example/just_jokes/MainActivity.java
+++ app/src/main/java/com/example/just_jokes/MainActivity.java
@@ -1,29 +1,48 @@
 package com.example.just_jokes;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView randomJokeTextView = findViewById(R.id.random_joke_text_view);
-        final TextView upvotesTextView = findViewById(R.id.upvotes_text_view);
-        final TextView downvotesTextView = findViewById(R.id.downvotes_text_view);
-        final Button randomJokeButton = findViewById(R.id.random_joke_button);
-        final Button upvoteButton = findViewById(R.id.upvote_button);
-        final Button downvoteButton = findViewById(R.id.downvote_button);
+        tabLayout=findViewById(R.id.tab_layout);
+        viewPager=findViewById(R.id.view_pager);
 
-        JokeRequestHandler jokeRequestHandler = new JokeRequestHandler(this,randomJokeTextView,upvotesTextView,downvotesTextView);
-        jokeRequestHandler.getRandomJoke();
-        jokeRequestHandler.setRandomJokeButtonOnClickListener(randomJokeButton);
-        jokeRequestHandler.setUpvoteButtonOnClickListener(upvoteButton);
-        jokeRequestHandler.setDownvoteButtonOnClickListener(downvoteButton);
+        final FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        viewPager.setAdapter(fragmentAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }

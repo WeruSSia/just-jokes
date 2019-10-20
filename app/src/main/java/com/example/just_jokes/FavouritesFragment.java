72f6376c530a4fc389e3a80b2495ef664f178723
Weronika Mirek <werussia16@gmail.com>
2019-10-20 18:26:09 +0200

    FavouritesFragment cleaned

--- app/src/main/java/com/example/just_jokes/FavouritesFragment.java
+++ app/src/main/java/com/example/just_jokes/FavouritesFragment.java
@@ -1,31 +1,19 @@
 package com.example.just_jokes;
 
-
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class FavouritesFragment extends Fragment {
 
-
     public FavouritesFragment() {
-        // Required empty public constructor
     }
-
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_favourites, container, false);
     }
-
 }

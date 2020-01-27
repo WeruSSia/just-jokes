fe7969942e99d92b4442c19779081666a3c5b30a
Weronika Mirek <werussia16@gmail.com>
2020-01-27 17:40:42 +0100

    FavouriteJokesListViewAdapter implementation

--- app/src/main/java/com/example/just_jokes/FavouriteJokesListViewAdapter.java
+++ app/src/main/java/com/example/just_jokes/FavouriteJokesListViewAdapter.java
@@ -1,4 +1,28 @@
 package com.example.just_jokes;
 
-public class FavouriteJokesListViewAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class FavouriteJokesListViewAdapter extends ArrayAdapter<JokeDto> {
+
+    public FavouriteJokesListViewAdapter(Context context, ArrayList<JokeDto> favouriteJokes) {
+        super(context, 0, favouriteJokes);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        JokeDto jokeDto = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.favourite_joke_item, parent, false);
+        }
+        TextView jokeContent = convertView.findViewById(R.id.joke_content);
+        jokeContent.setText(jokeDto.getContent());
+        return convertView;
+    }
 }
